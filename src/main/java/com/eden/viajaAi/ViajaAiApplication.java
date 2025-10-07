bf78a5c555902abf97480724c5ff1262138e1613
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2025-10-07 04:58:06 -0300

    first

---
+++ src/main/java/com/eden/viajaAi/ViajaAiApplication.java
@@ -0,0 +1,13 @@
+package com.eden.viajaAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eden/viajaAi/ViajaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden.viajaAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
