2a499d52852e0fe0e396c002325723e87bb7f48d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2025-11-30 21:44:47 -0300

    isolate get and post functions

--- backend/src/main/java/com/eden/bonvoyage/http/controllers/room/RoomController.java
+++ backend/src/main/java/com/eden/bonvoyage/http/controllers/room/RoomController.java
@@ -6,10 +6,7 @@ import com.eden.bonvoyage.core.repository.RoomRepository;
 import com.eden.bonvoyage.core.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,10 +17,15 @@ public class RoomController {
   @Autowired
   RoomService roomService;
 
-  @GetMapping
-  List<Room> find(@Nullable @RequestBody RoomFilter filter) {
+  @PostMapping
+  List<Room> findFilter(@RequestBody RoomFilter filter) {
     if  (filter == null) return roomService.findAll();
     return roomService.findByFilters(filter);
+  }
+
+  @GetMapping
+  List<Room> findAll() {
+    return roomService.findAll();
   }
 
 }
